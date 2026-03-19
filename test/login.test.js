097df6f2ef9bfe83feff8ef4bea2c55251c6d333
Julia Strapassan <juliastrapa@gmail.com>
2026-03-19 15:23:03 -0300

    Adicionando teste de login

--- test/login.test.js
+++ test/login.test.js
@@ -6,14 +6,14 @@ describe('Login', () =>{
         it('Deve retornar 200 com um token em string quando usar credenciais válidas', async () =>{
             const resposta =  await request('http://localhost:3000')
                .post('/login')
-               .set('Content-Type', 'aplicattion/json')
+               .set('Content-Type', 'application/json')
                .send({
                 'username': 'julio.lima',
                 'senha': '123456'
                })
 
             expect(resposta.status).to.equal(200);
-            expect(Response.body.token).to.be.a('string');
+            expect(resposta.body.token).to.be.a('string');
 
 
         })
